0b4ca5b6463d8d44c5fe72b95a81eee26f323460
Luca L. Bow <luca@doubleelbow.com>
2018-08-21 22:30:31 +0200

    Remove lein-tools-deps

--- project.clj
+++ project.clj
@@ -3,7 +3,15 @@
   :url "https://github.com/doubleelbow/capital-http"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-tools-deps "0.4.1"]]
-  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
-  :lein-tools-deps/config {:config-files [:install :user :project]}
-  :profiles {:dev {:lein-tools-deps/config {:aliases [:dev]}}})
+  :dependencies [[com.doubleelbow.capital/capital "0.1.0-SNAPSHOT"]
+                 [io.pedestal/pedestal.log "0.5.4"]
+                 [clj-http "3.9.1"]
+                 [pathetic "0.5.1"]]
+  :profiles {:dev {:source-paths ["dev"]
+                   :dependencies [[org.clojure/tools.namespace "0.3.0-alpha1"]
+                                  [ch.qos.logback/logback-classic "1.1.7" :exclusions [org.slf4j/slf4j-api]]
+                                  [org.slf4j/jul-to-slf4j "1.7.21"]
+                                  [org.slf4j/jcl-over-slf4j "1.7.21"]
+                                  [org.slf4j/log4j-over-slf4j "1.7.21"]
+                                  [org.clojure/data.json "0.2.6"]
+                                  [clj-http-fake "1.0.3"]]}})
